f320cb0c5f15cd52a68f375fbb4b6bdfdac40947
EmirKosekul <88345933+EmirKosekul@users.noreply.github.com>
2024-03-15 01:50:24 +0300

    website question generator

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import random
 from flask import Flask, jsonify, render_template, request
 
 from transformers import pipeline
@@ -5,10 +6,11 @@ from transformers import pipeline
 app = Flask(__name__)
 
 summarizer = pipeline("summarization", model="Falconsai/text_summarization")
-
+question_generator = pipeline("text2text-generation", model="iarfmoose/t5-base-question-generator")
 
 main_text=""
 result=""
+header=""
 @app.route('/orjinal', methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
@@ -25,12 +27,16 @@ def index():
         selected_text = request.form['gosterilenMetin']
         main_text=request.form['metinAlani']
         if request.form['action'] == 'first_action':
-         #result=summarizer(selected_text, max_length=1000, min_length=30, do_sample=False)[0]['summary_text']
-         result="ilk işlem"   
+         header="Your summary is here!"
+         result=summarizer(selected_text, max_length=1000, min_length=30, do_sample=False)[0]['summary_text']
+         #result="ilk işlem"   
         elif request.form['action'] == 'second_action':
-         
-         result="ikinci işlem"   
-        return render_template('rightclick.html', selected_text=selected_text,main_text=main_text ,result=result)
+         header="A question for you!"
+         random_number = random.randint(1, 5)         
+         generated_questions = question_generator(selected_text, max_length=50, num_return_sequences=1, num_beams=5, early_stopping=True)
+         question=generated_questions[1]
+         result=question   
+        return render_template('rightclick.html', selected_text=selected_text,main_text=main_text ,result=result, header=header)
     return render_template('rightclick.html')
 
 if __name__ == '__main__':
